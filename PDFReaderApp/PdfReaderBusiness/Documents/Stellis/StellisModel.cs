8909dbf8936ac80b933a4331d39f7b689a614387
Ryan Lintag <77764315+ryanlintag-who@users.noreply.github.com>
2024-10-09 15:42:51 +0800

    Completed reading the Work Experience and Education/Training/Qualifications

--- PDFReaderApp/PdfReaderBusiness/Documents/Stellis/StellisModel.cs
+++ PDFReaderApp/PdfReaderBusiness/Documents/Stellis/StellisModel.cs
@@ -9,10 +9,15 @@ namespace PdfReaderBusiness.Documents.Stellis
             Position = new PositionStellisModel();
             PersonalInfo = new CandidatePersonalInfoStellisModel();
             BasicInfo = new BasicInfoStellisModel();
+            WorkExperiences = new List<WorkExperienceStellisModel>();
+            EducationTrainingQualifications = new List<EducationTrainingQualification>();
         }
         public PositionStellisModel Position { get; set; }
         public CandidatePersonalInfoStellisModel PersonalInfo { get; set; }
         public BasicInfoStellisModel BasicInfo { get; set; }
+
+        public List<EducationTrainingQualification> EducationTrainingQualifications { get; set; }
+        public List<WorkExperienceStellisModel> WorkExperiences { get; set; }
     }
 
     public class BasicInfoStellisModel
@@ -50,5 +55,72 @@ namespace PdfReaderBusiness.Documents.Stellis
     {
         public string LanguageName { get; set; }
         public string LanguageProficiency { get; set; }
+    }
+
+    public class WorkExperienceStellisModel
+    {
+        public string JobTitle { get; set; }
+        public string WorkSchedule { get; set; }
+        public string StartDate { get; set; }
+        public string TypeOfBusiness { get; set; }
+        public string MajorOffice { get; set; }
+        public string DepartmentDivision { get; set; }
+        public string DutyStation { get; set; }
+        public string ContractType { get; set; }
+        public string Grade { get; set; }
+        public string SupervisorName { get; set; }
+        public string SupervisorTitle { get; set; }
+        public string SupervisorPhone { get; set; }
+        public string SupervisorEmail { get; set; }
+        public string AreaOfWork { get; set; }
+        public string NatureOfWork { get; set; }
+        public string DutiesAndResponsibilities { get; set; }
+        public string Achievements { get; set; }
+    }
+    public abstract class EducationTrainingQualification
+    {
+        public string EducationType { get; private set; }
+
+        protected EducationTrainingQualification(string educationType)
+        {
+            EducationType = educationType;
+        }
+
+        public abstract void AddString(string current);
+    }
+    public sealed class Education : EducationTrainingQualification
+    {
+        public Education(string educationType) : base(educationType)
+        {
+        }
+        private List<string> items { get; set; } = new List<string>();
+        public override void AddString(string current)
+        {
+            items.Add(current);
+        }
+    }
+    public sealed class Training : EducationTrainingQualification
+    {
+        public Training(string educationType) : base(educationType)
+        {
+        }
+
+        private List<string> items { get; set; } = new List<string>();
+        public override void AddString(string current)
+        {
+            items.Add(current);
+        }
+    }
+    public sealed class Qualification : EducationTrainingQualification
+    {
+        public Qualification(string educationType) : base(educationType)
+        {
+        }
+
+        private List<string> items { get; set; } = new List<string>();
+        public override void AddString(string current)
+        {
+            items.Add(current);
+        }
     }
 }
